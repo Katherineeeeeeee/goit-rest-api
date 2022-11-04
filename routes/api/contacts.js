d0db2acc0079fe7139869e358e8a715db4d5ec5c
Katherineeeeeeee <katherine.dudar.7@gmail.com>
2022-11-04 18:26:29 +0200

    worked on 3 hw

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,22 +1,30 @@
 const express = require("express");
 const ctrl = require("../../controllers/contacts");
 const { ctrlWrapper } = require("../../helpers");
-const { validateBody } = require("../../middlewares");
-const shema = require("../../shemas/contactShema");
+const { validateBody, isValidId } = require("../../middlewares");
+const { shemas } = require("../../models/contact");
 const router = express.Router();
 
 router.get("/", ctrlWrapper(ctrl.getAll));
 
-router.get("/:contactId", ctrlWrapper(ctrl.getById));
+router.get("/:contactId", isValidId, ctrlWrapper(ctrl.getById));
 
-router.post("/", validateBody(shema.contactSchema), ctrlWrapper(ctrl.add));
+router.post("/", validateBody(shemas.joiContactSchema), ctrlWrapper(ctrl.add));
 
-router.delete("/:contactId", ctrlWrapper(ctrl.remove));
+router.delete("/:contactId", isValidId, ctrlWrapper(ctrl.remove));
 
 router.put(
   "/:contactId",
-  validateBody(shema.contactSchema),
+  isValidId,
+  validateBody(shemas.joiContactSchema),
   ctrlWrapper(ctrl.updateById)
+);
+
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  validateBody(shemas.updateFavoriteSchema),
+  ctrlWrapper(ctrl.updateFavorite)
 );
 
 module.exports = router;
